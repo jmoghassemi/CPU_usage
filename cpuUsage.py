708399bb04a2cacd46adf7cfe15fea365bc02dd9
MHL <paramont@gmail.com>
2021-10-15 20:39:52 +0200

    Some clearing and fixing files

--- cpuUsage.py
+++ cpuUsage.py
@@ -5,44 +5,27 @@ import os
 import sys
 import argparse
 import time
+from datetime import datetime
+import functions as func
 
 ########## Variable Definition ################
 default_limit = 60.0
 default_min=3
 
-######### Function Definition ################
-def get_total_mem():
-	total_raw_mem=os.popen('cat /proc/meminfo | grep MemTotal').read().split()
-	return int(total_raw_mem[1])/1024/1024
-
-def check_free_mem_per():
-  mem_raw=os.popen('free -mw | grep Mem').read().split()
-  free_mem_percent="{:.2}".format(int(mem_raw[3])/int(mem_raw[1]))
-  print(f"Total Memory: {mem_raw[1]}")
-  print(f"Free Memory: {mem_raw[3]}")
-  print(f"Free Memory Percent: {free_mem_percent}%")
-  print("#############################################")
-  return float(free_mem_percent)
-
-def check_cpu():
-	mpstat_raw=os.popen('mpstat | grep all').read().split()
-	print(f"IO wait %: {mpstat_raw[5]}")
-	print(f"Idle %: {mpstat_raw[11]}")
-	
 ########## Main Section ####################
 if __name__ == "__main__":
     
 	parser = argparse.ArgumentParser(description='CPU usage and Memory check. This program can using a serviced.')
 	
-	parser.add_argument('byte_limit_value', type=float, nargs='?', help=f'specifies when the script should respond (create alert sound and write status report). \n Default value for this action is 60%% of {int(get_total_mem())} GB.\n Accepted value between 10 to 99.9', default=default_limit)
+	parser.add_argument('byte_limit_value', type=float, nargs='?', help=f'specifies when the script should respond (create alert sound and write status report). \n Default value for this action is 60%% of {int(func.get_total_mem())} GB.\n Accepted value between 10 to 99.9', default=default_limit)
 
 	parser.add_argument('minutes', type=int, nargs='?', help='indicates at what intervals in minutes the script should check CPU usage.\n Default value is 3 minutes. \nAccepted value between 1 to 60.', default=default_min)
 	
 	args = parser.parse_args()
 	
-	print("Argument values:")
-	print(args.byte_limit_value)
-	print(args.minutes)
+#	print("Argument values:")
+#	print(args.byte_limit_value)
+#	print(args.minutes)
 
 	if args.byte_limit_value > 99.99 or args.byte_limit_value < 0:
 		parser.error("byte_limit_value cannot be larger than 99.99 and less than 0.")
@@ -50,7 +33,8 @@ if __name__ == "__main__":
 		parser.error("minutes cannot be larger than 60 and less than 1.")
 
 	while True: 
-		if check_free_mem_per()>args.byte_limit_value:
+		if func.check_free_mem_per()>args.byte_limit_value:
 			print(f"Start to monitor system each {args.minutes} minutes. make alert wall")
+			func.cpu_rapport()
 			time.sleep(args.minutes*60)
 
---
+++ functions.py
@@ -0,0 +1,46 @@
+import os
+from datetime import datetime
+
+
+def cpu_rapport():
+	date = datetime.now().strftime("%Y-%m-%d")
+	full_time = datetime.now().strftime("%H:%M:%S")
+
+	cpu_filename = 'cpu_' + date + '.log'
+	f = open(cpu_filename, "a")
+	f.write(full_time + '\n')
+
+	ps_output = os.popen('ps -o pid,user,%mem,command ax | grep -v PID').read().splitlines()
+
+	tmp_txt = ''
+
+	for item in ps_output:
+		tx = item.split()
+		# wf = ps_output[item] +','+ ps_output[item+1] + ',' + ps_output[item+2] + '\n'
+		if tx[2] != '0.0':
+			wf = tx[0] + ',' + tx[1] + ',' + tx[2] + ','
+			if len(tx) > 3:
+				for i in range(3, len(tx) - 1):
+					tmp_txt += tx[i] + ' '
+				wf += tmp_txt
+		f.write(wf + '\n')
+	f.close()
+
+def get_total_mem():
+  total_raw_mem=os.popen('cat /proc/meminfo | grep MemTotal').read().split()
+  return int(total_raw_mem[1])/1024/1024
+
+def check_free_mem_per():
+  mem_raw=os.popen('free -mw | grep Mem').read().split()
+  free_mem_percent="{:.2}".format(int(mem_raw[3])/int(mem_raw[1]))
+#  print(f"Total Memory: {mem_raw[1]}")
+#  print(f"Free Memory: {mem_raw[3]}")
+#  print(f"Free Memory Percent: {free_mem_percent}%")
+#  print("#############################################")
+  return float(free_mem_percent)
+
+def check_cpu():
+  mpstat_raw=os.popen('mpstat | grep all').read().split()
+#  print(f"IO wait %: {mpstat_raw[5]}")
+#  print(f"Idle %: {mpstat_raw[11]}")
+
