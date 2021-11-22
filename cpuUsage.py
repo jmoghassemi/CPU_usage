d9ea0c153f63b55b1b69600da0b91c777799247b
hadi-tghl <hadi@gmx.se>
2021-11-22 14:00:00 +0100

    Edited some comments

--- cpuUsage.py
+++ cpuUsage.py
@@ -15,11 +15,11 @@ default_min=3
 ########## Main Section ####################
 if __name__ == "__main__":
     
-	parser = argparse.ArgumentParser(description='This program monitors CPU and Memory usage via a specially designed service file.')
+	parser = argparse.ArgumentParser(description='This program monitors CPU and Memory usage via the specially designed service file.')
 	
-	parser.add_argument('byte_limit_value', type=int, nargs='?', help=f'This parameter specifies the critial threshold for memory usage. Once memory usage exceeds this value, the python script starts to create a status report, which by default is saved at /etc/cpuUsage. \n The Default value for byte_limit_value is 75%% of {int(func.get_total_mem())} GB.\n byte_limit_value needs to be a positive integer and in MB.', default=default_limit)
+	parser.add_argument('byte_limit_value', type=int, nargs='?', help=f'This parameter specifies the critial thresholder of the memory usage. Once memory usage exceeds the value 75% of the system memory then script starts to create a status report and saves in /etc/cpuUsage/report.\n The Default value for byte_limit_value is 75% of {int(func.get_total_mem())} GB.\n byte_limit_value needs to be a positive integer and in MB.', default=default_limit)
 
-	parser.add_argument('minutes', type=int, nargs='?', help='This parameter indicates at what time intervals the script needs to check memory usage and is measured in minutes.\n The default value is 3 minutes. \nThe accepted values for this variable range from 1 to 60 minutes.', default=default_min)
+	parser.add_argument('minutes', type=int, nargs='?', help='This parameter indicates the time intervals that script needs to check the memory usage and it''s measured in minutes.\n The default value is 3 minutes. \nThe accepted values for this variable range from 1 to 60 minutes.', default=default_min)
 	
 	args = parser.parse_args()
 	
