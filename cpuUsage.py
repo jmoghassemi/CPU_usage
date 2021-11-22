99880cba224a47ad9658d6b3ce55b5d6e3ea4cfe
hadi-tghl <hadi@gmx.se>
2021-11-22 15:25:24 +0100

    Edited some comments

--- cpuUsage.py
+++ cpuUsage.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     
 	parser = argparse.ArgumentParser(description='This program monitors CPU and Memory usage via the specially designed service file.')
 	
-	parser.add_argument('byte_limit_value', type=int, nargs='?', help=f'This parameter specifies the critial thresholder of the memory usage. Once memory usage exceeds the value 75% of the system memory then script starts to create a status report and saves in /etc/cpuUsage/report.\n The Default value for byte_limit_value is 75% of {int(func.get_total_mem())} GB.\n byte_limit_value needs to be a positive integer and in MB.', default=default_limit)
+	parser.add_argument('byte_limit_value', type=int, nargs='?', help=f'This parameter specifies the critial thresholder of the memory usage. Once memory usage exceeds the value 75% of the system memory then script starts to create a status report and saves in /etc/cpuUsage/report.\n The Default value for byte_limit_value is 75% of {int(func.get_total_mem())} GB.\n byte_limit_value needs to be a positive integer in MB.', default=default_limit)
 
 	parser.add_argument('minutes', type=int, nargs='?', help='This parameter indicates the time intervals that script needs to check the memory usage and it''s measured in minutes.\n The default value is 3 minutes. \nThe accepted values for this variable range from 1 to 60 minutes.', default=default_min)
 	
